00dbc6a89fed856a0fa9c39da8c794cf1fe16ca4
sickdyd <roberto.reale.ja@gmail.com>
2020-05-04 14:09:45 +0900

    Updates

---
+++ precache-manifest.cdaa9d527391d9fb3c1f39507641ae3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "986900e602ae7999827767bc92236187",
+    "url": "/booking-manager-frontend/index.html"
+  },
+  {
+    "revision": "16d9b723d36316f52cbf",
+    "url": "/booking-manager-frontend/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "dab5d146632ec3736ac6",
+    "url": "/booking-manager-frontend/static/css/main.f9c62464.chunk.css"
+  },
+  {
+    "revision": "16d9b723d36316f52cbf",
+    "url": "/booking-manager-frontend/static/js/2.8385c714.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/booking-manager-frontend/static/js/2.8385c714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab5d146632ec3736ac6",
+    "url": "/booking-manager-frontend/static/js/main.28d2bea0.chunk.js"
+  },
+  {
+    "revision": "de399203bd23cc016647",
+    "url": "/booking-manager-frontend/static/js/runtime-main.511b6c0c.js"
+  }
+]);
