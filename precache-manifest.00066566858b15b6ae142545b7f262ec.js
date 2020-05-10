83c0b4a40b14bbe758212f0f59b729a7c2cbbcdc
sickdyd <roberto.reale.ja@gmail.com>
2020-05-10 15:34:49 +0900

    Updates

--- precache-manifest.00066566858b15b6ae142545b7f262ec.js
+++ precache-manifest.00066566858b15b6ae142545b7f262ec.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8565336fd5bd621d471877364fb99914",
+    "revision": "a03372c1b09c6a3e4cf98df1e68ce514",
     "url": "/booking-manager-frontend/index.html"
   },
   {
-    "revision": "c00343f92824233f8f4a",
+    "revision": "616f976cc05e4e69b698",
     "url": "/booking-manager-frontend/static/css/2.a72c35d3.chunk.css"
   },
   {
-    "revision": "3b89a73cf638df5bba46",
+    "revision": "9a551e7a9d1bec025703",
     "url": "/booking-manager-frontend/static/css/main.a679b4f2.chunk.css"
   },
   {
-    "revision": "c00343f92824233f8f4a",
-    "url": "/booking-manager-frontend/static/js/2.a22ec1cc.chunk.js"
+    "revision": "616f976cc05e4e69b698",
+    "url": "/booking-manager-frontend/static/js/2.22204daa.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/booking-manager-frontend/static/js/2.a22ec1cc.chunk.js.LICENSE.txt"
+    "url": "/booking-manager-frontend/static/js/2.22204daa.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3b89a73cf638df5bba46",
-    "url": "/booking-manager-frontend/static/js/main.f9747316.chunk.js"
+    "revision": "9a551e7a9d1bec025703",
+    "url": "/booking-manager-frontend/static/js/main.12714f83.chunk.js"
   },
   {
     "revision": "de399203bd23cc016647",
