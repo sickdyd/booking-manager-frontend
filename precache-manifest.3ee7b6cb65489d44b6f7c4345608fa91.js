63b67e2a48aad6216a51d246e308935f46f299fc
sickdyd <roberto.reale.ja@gmail.com>
2020-05-11 21:00:36 +0900

    Updates

---
+++ precache-manifest.3ee7b6cb65489d44b6f7c4345608fa91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d3e35faf4ee7ae8704bc6f73c3f265",
+    "url": "/booking-manager-frontend/index.html"
+  },
+  {
+    "revision": "74bc5ae9f89d95858c55",
+    "url": "/booking-manager-frontend/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "ba2b1410a3f913530952",
+    "url": "/booking-manager-frontend/static/css/main.fa699386.chunk.css"
+  },
+  {
+    "revision": "74bc5ae9f89d95858c55",
+    "url": "/booking-manager-frontend/static/js/2.339a08bc.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/booking-manager-frontend/static/js/2.339a08bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba2b1410a3f913530952",
+    "url": "/booking-manager-frontend/static/js/main.56e2b021.chunk.js"
+  },
+  {
+    "revision": "de399203bd23cc016647",
+    "url": "/booking-manager-frontend/static/js/runtime-main.511b6c0c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/booking-manager-frontend/precache-manifest.8ee090c44252b2b322d8f57d5bc42bf0.js"
+  "/booking-manager-frontend/precache-manifest.3ee7b6cb65489d44b6f7c4345608fa91.js"
 );
 
 self.addEventListener('message', (event) => {
